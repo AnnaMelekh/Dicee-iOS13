2d1634a181fb41c41690e913a2e006dbac3a4556
AnnaMelekh <annasmelekhina@gmail.com>
2024-09-13 21:54:55 +0200

    ios bootcamp course Dicee App

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = [
+        UIImage(named: "DiceOne")!,
+        UIImage(named: "DiceTwo")!,
+        UIImage(named: "DiceThree")!,
+        UIImage(named: "DiceFour")!,
+        UIImage(named: "DiceFive")!,
+        UIImage(named: "DiceSix")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got tapped")
+        
+        //        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+        diceImageView1.image = diceArray[Int.random(in: 0..<diceArray.count)]
+        
+    }
 }
+
+
 
